df3402607a126259a04b1e9a467b75ac3f199765
muhammad <memomq70@gmail.com>
2024-07-03 11:10:45 +0300

    updated instance type

--- tf/EKS/node-group.tf
+++ tf/EKS/node-group.tf
@@ -3,7 +3,6 @@ resource "aws_eks_node_group" "main" {
   node_group_name = "main"
   node_role_arn   = aws_iam_role.worker-node-role.arn
   subnet_ids      = [var.private_subnet_ids[0],var.private_subnet_ids[1]]
-  instance_types = ["t3.large"]
   tags = {
     "Name" = "mf-cluster-main-node-group"
   }
